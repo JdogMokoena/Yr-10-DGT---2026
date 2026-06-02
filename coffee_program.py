b67073a71ae938ce79042381d387d6088ebe4abf
JdogMokoena <jason.mokoena@kingsway.school.nz>
2026-06-03 09:57:24 +1200

    Add files via upload

---
+++ coffee_program.py
@@ -0,0 +1,24 @@
+#Demonsrate how to use an IF statement
+#Authoer : H=Juergen Lier
+# 1 May 2026 
+# Version 1
+
+'''TODO: Create a program that will ask the user whether
+ they like coffee or not. If they do not like coffee,
+ I will try to persuade them to try it.'''  
+# Coffee program. Checking whether you like Coffee
+# Checking wehether you like coffee and stroes the answer in 
+# a variable.
+like_coffee = input("Do you like coffee?")
+# print(like_coffee)
+
+if like_coffee == "yes":
+    print("That is great! I like coffee too!")
+
+elif like_coffee == "no":
+ 
+ print("You are missing out. Why not give it a try?")
+ 
+
+   
+        
